58f851bbc8ba694064ead5754b8b25b1ce89d922
Cameron Baumgartner <cameron.baumgartner@vcfa.edu>
2021-03-02 19:59:01 -0500

    set up form state handling

--- client/components/SearchBar.js
+++ client/components/SearchBar.js
@@ -2,13 +2,48 @@ import React from 'react';
 // import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 // import 'react-tabs/style/react-tabs.css';
 
+// on form submit:
+// send a GET request to '/politicians/' with the user-entered address concatenated on
+
 const SearchBar = () => {
+  const [address, setAddress] = useState('');
+
+  const onChange = (e) => setAddress(e.target.value);
+
+  const onSubmit = (e) => {
+    if (e.key === 'Enter' || e.type === 'click') {
+      e.preventDefault(); // prevent page refesh
+    }
+    console.log('Form state:', address);
+    // don't submit an empty string
+    if (!address) return;
+
+    const codesmith = '68 White St 5th floor, New York, NY 10013';
+
+    fetch('/politicians/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        address: codesmith,
+      })
+    })
+
+  }
+
   return (
     <div className="searchBarContainer">
       <div className="formContainer">
         <form className="form">
-          <input className="formField" type="input" id="search" placeholder="Search With Zipcode..." />
-          <input className="searchSubmit" type="submit" value="Submit" />
+          <label>Find your representatives: 
+            <input className="formField" 
+            type="text" 
+            id="search" 
+            placeholder="Home address..."
+            onChange={onChange} />
+          </label>
+          <button className="searchSubmit" onSubmit={onSubmit} />
         </form>
       </div>
     </div>
