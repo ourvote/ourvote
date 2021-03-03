b6dbf0293877370a19ba96d3db5b92f467e8c67d
Cameron Baumgartner <cameron.baumgartner@vcfa.edu>
2021-03-03 17:17:09 -0500

    added links to scandals and donors

--- client/components/Politician.js
+++ client/components/Politician.js
@@ -12,6 +12,7 @@ const Politician = ({ name, office, division, party, website, phone, email, phot
         <h4 className="contact" >Website: <a href={website}>{website || 'None provided'}</a></h4>
         <h4 className="contact" >Phone: {phone}</h4>
         <h4 className="contact" >Email: {email}</h4>
+        <p>See this politician's <a href={`https://www.google.com/search?q=${name}%20scandals`} target="_blank" rel="noopener noreferrer">scandals</a> and <a href={`https://www.opensecrets.org/search?q=${name}`} target="_blank" rel="noopener noreferrer">donors</a>.</p>
       </div>
     </div>
   )
