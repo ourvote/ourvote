c12da886f7dc4ddca8b65e1ae0a0781e1ddc788b
Cameron Baumgartner <cameron.baumgartner@vcfa.edu>
2021-03-04 15:17:36 -0500

    bug fix for names containing double quotes

--- __tests__/db.js
+++ __tests__/db.js
@@ -6,16 +6,6 @@ const {
   assembleSql
 } = helpers;
 
-/*
-test database in Elephant
-in model, check node environment. if TEST, use the test db uri
-
-afterAll(async () => await db.end());
-
-//in model.js
-db.end = pool.end.bind(pool)
-*/
-
 describe('Database unit tests', () => {
 
   beforeAll(()=> {
@@ -25,6 +15,7 @@ describe('Database unit tests', () => {
     }
     else console.log(`Running unit tests on database in ${process.env.NODE_ENV} environment.`);
 
+    // TODO: use async/await to ensure this executes
     db.query('TRUNCATE politicians RESTART IDENTITY CASCADE', (error, response) => {
       if (error) console.log('Problem truncating test db:', error);
       console.log('Truncated mock db');
@@ -44,6 +35,7 @@ describe('Database unit tests', () => {
     db.end();
   });
 
+  // TODO: use async/await to ensure the assertions get checked
   it('Test database should have the right tables', () => {
     const query = `SELECT table_name
     FROM information_schema.tables
@@ -60,28 +52,102 @@ describe('Database unit tests', () => {
 
   describe('Helper functions', () => {
     describe('escapeQuotes', () => {
-      xit('', () => {
-     
-      })
-    });
-
-    describe('arrayToSqlList', () => {
       it('should escape apostrophes within the string by inserting an extra apostrophe', () => {
         const result = escapeQuotes("Commonwealth's Attorney");
 
         expect(result).toEqual("Commonwealth''s Attorney");
       })
+
+      it('should not escape pairs of double quotes within the string', () => {
+        const result = escapeQuotes(`Gerald "Gerry" O'Connelly`);
+
+        expect(typeof result).toEqual('string');
+        expect(result).toEqual(`Gerald "Gerry" O''Connelly`);
+      })
+    });
+
+    describe('arrayToSqlList', () => {
+      it('should convert an array of strings to a SQL-formatted list of values', () => {
+        const result = arrayToSqlList(['Joe Biden', 'President', 'joe@potus.org']);
+
+        expect(result).toEqual("('Joe Biden', 'President', 'joe@potus.org')")
+      })
     });
 
     describe('assembleSql', () => {
-      xit('', () => {
-     
+      const apiJsonResp = {
+        "offices": [
+          {
+            "name": "President of the United States",
+            "divisionId": "ocd-division/country:us",
+            "levels": [
+              "country"
+            ],
+            "roles": [
+              "headOfState",
+              "headOfGovernment"
+            ],
+            "officialIndices": [
+              0
+            ]
+          },
+        ],
+        "officials": [
+          {
+            "name": "Joseph R. Biden",
+            "address": [
+              {
+                "line1": "1600 Pennsylvania Avenue Northwest",
+                "city": "Washington",
+                "state": "DC",
+                "zip": "20500"
+              }
+            ],
+            "party": "Democratic Party",
+            "phones": [
+              "(202) 456-1111"
+            ],
+            "urls": [
+              "https://www.whitehouse.gov/"
+            ],
+            "channels": [
+              {
+                "type": "Twitter",
+                "id": "potus"
+              }
+            ]
+          }
+        ],      
+      }
+
+      it('should turn a JSON object into a SQL-formatted list of values', () => {
+        const result = assembleSql(apiJsonResp);
+
+        expect(typeof result).toEqual('string');
+        expect(result).toEqual(
+          "('President of the United States', 'ocd-division/country:us', 'Joseph R. Biden', 'Democratic Party', null, 'https://www.whitehouse.gov/', '(202) 456-1111', null)"
+        )
       })
     });
   })
   
   
   describe('Politicians table', () => {
+    // TODO: use async/await to ensure the assertions get checked
+    it('should should have the right columns', () => {
+      const query = `SELECT COLUMN_NAME
+      FROM INFORMATION_SCHEMA.COLUMNS
+      WHERE TABLE_NAME = 'politicians'
+      ORDER BY ORDINAL_POSITION;`;
+  
+      db.query(query, (error, response) => {
+        if (error) console.log('Problem querying for columns in politicians table:', error);
+  
+        expect(response.rows).toHaveLength(9);
+        expect(response.rows).toEqual(['users', 'prev_registrations', 'elections', 'roles', 'donors', 'politicians', 'pols_roles', 'donations', 'candidates']);
+      });
+    })
+
     xit('should be able to write perfectly shaped data', () => {
       // write perfectly shaped data to the database
       // run a SELECT
@@ -105,7 +171,7 @@ describe('Database unit tests', () => {
     })
   })
 
-  describe('searchController.getAll', () => {
+  xdescribe('searchController.getAll', () => {
     xit('should return as many rows as exist on "politicians"', () => {
      
     })
@@ -115,7 +181,7 @@ describe('Database unit tests', () => {
     })
   })
 
-  describe('searchController.upsertByAddress', () => {
+  xdescribe('searchController.upsertByAddress', () => {
     xit('should query Google Civic Info API with address sent on request query', () => {
      
     })
@@ -133,7 +199,7 @@ describe('Database unit tests', () => {
     })
   })
 
-  describe('searchController.getByNames', () => {
+  xdescribe('searchController.getByNames', () => {
     xit('should return a politician record for every name passed on res.locals.names', () => {
      
     })
--- server/models/model.js
+++ server/models/model.js
@@ -2,6 +2,8 @@ const { Pool } = require('pg');
 
 let connectionString = '';
 if (process.env.NODE_ENV === 'test') {
+  // make .env available to this file when not using node start script
+  require('dotenv').config();
   connectionString = process.env.TEST_DB;
 }
 else connectionString = process.env.DB_URI;
