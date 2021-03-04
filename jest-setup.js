8c342cebb658aadb0aabe7bb6e0a840dfe44701f
Cameron Baumgartner <cameron.baumgartner@vcfa.edu>
2021-03-04 10:22:43 -0500

    removed jest config to avoid opening 3000 port

--- jest-setup.js
+++ jest-setup.js
@@ -1,3 +1,12 @@
 module.exports = async () => {
   global.testServer = await require('./server/server.js');
 };
+
+/*
+To use this config, add the following to package.json:
+
+  "jest": {
+    "globalSetup": "./jest-setup.js",
+    "globalTeardown": "./jest-teardown.js"
+  },
+*/
